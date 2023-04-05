3478445c5875604ccd0ada60b14a7662149c5c8a
kumneger Wondimu <93655689+Kumneger0@users.noreply.github.com>
2023-04-05 21:27:50 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,10 @@
-import Header from "./components/header/header";
 import Querywrapper from "./components/query-wraper/Querywrapper";
-import Footer from "./components/footer/footer";
-
 export default function Home() {
   return (
     <>
-      <section>
-        <Header />
-      </section>
       <main>
         <Querywrapper />
       </main>
-      <footer>
-        <Footer />
-      </footer>
     </>
   );
 }
